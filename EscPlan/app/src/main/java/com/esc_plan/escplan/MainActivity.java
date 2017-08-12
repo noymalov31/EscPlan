9122beccdb56ffbf2aac802989c132cdb4f78c84
nisan chiprut <nisan.chiprut@mail.huji.ac.il>
2017-08-12 19:10:27 +0300

    Adding Firebase db

--- EscPlan/app/src/main/java/com/esc_plan/escplan/MainActivity.java
+++ EscPlan/app/src/main/java/com/esc_plan/escplan/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
+
+    DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
